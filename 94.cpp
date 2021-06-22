6103ca2d424d2c56661a1d5184dfe6fa010c1317
Chang-Bin Zhang <35215543+zhangchbin@users.noreply.github.com>
2021-06-22 17:13:33 +0800

    Create 94.cpp

---
+++ 94.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    void dfs(TreeNode* tmp){
+        if(tmp->left != nullptr) dfs(tmp->left);
+        ans.push_back(tmp->val);
+        if(tmp->right != nullptr) dfs(tmp->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root != nullptr) dfs(root);
+        return ans;
+    }
+};
