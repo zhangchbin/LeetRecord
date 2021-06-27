22ef943abb5019b9cb3bece933c567bfb7777277
Chang-Bin Zhang <35215543+zhangchbin@users.noreply.github.com>
2021-06-27 18:10:14 +0800

    Create 461.cpp

---
+++ 461.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        while(x || y){
+            if((x&1)!=(y&1)) ans++;
+            x >>= 1;
+            y >>= 1;
+        }
+        return ans;
+
+    }
+};
