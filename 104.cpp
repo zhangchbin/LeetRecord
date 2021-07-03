601c8b7b7ea4cd2f5505a19a09e5aa4174583160
Chang-Bin Zhang <35215543+zhangchbin@users.noreply.github.com>
2021-07-03 22:12:56 +0800

    Create 104.cpp

---
+++ 104.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* p){
+        if(p == nullptr) return 0;
+        return max(dfs(p->left), dfs(p->right)) + 1;
+    }
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
+};
