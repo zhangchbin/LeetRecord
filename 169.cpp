fdd1d536e70a100bb31fc0b338ed8f376195dec0
Chang-Bin Zhang <35215543+zhangchbin@users.noreply.github.com>
2021-07-03 22:27:41 +0800

    Create 169.cpp

---
+++ 169.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int ans;
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(!cnt) ans=nums[i];
+            if(nums[i]==ans) cnt++;
+            else cnt--;
+        }
+        return ans;
+    }
+};
